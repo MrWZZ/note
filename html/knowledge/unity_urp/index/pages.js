9bd2e37d3f69dcf7bd5afc5ffc336100b9307e14
wzz <872173938@qq.com>
2022-10-25 01:27:14 +0800

    水面效果

--- html/knowledge/unity_urp/index/pages.js
+++ html/knowledge/unity_urp/index/pages.js
@@ -1,5 +1,5 @@
 var pages = [
-    {name : "API", url:"../pages/api.html"},
+    //{name : "API", url:"../pages/api.html"},
     {name : "BlinnPhong", url:"../pages/blinn_phong.html"},
     {name : "基本纹理", url:"../pages/base_tex.html"},
     {name : "法线贴图", url:"../pages/normal_map.html"},
@@ -7,4 +7,5 @@ var pages = [
     {name : "立方体纹理", url:"../pages/cube_map_reflection.html"},
     {name : "屏幕像素适配描边", url:"../pages/screen_back_outline.html"},
     {name : "卡通类渲染", url:"../pages/toon.html"},
+    {name : "卡通湖泊", url:"../pages/toon_sea.html"},
 ];
